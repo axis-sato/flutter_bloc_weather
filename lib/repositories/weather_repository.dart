a6185db0616598aa342ffaba53fca1e11014a68e
sato <sato_m@mediaxis.jp>
2019-12-25 20:37:16 +0900

    Repository

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc_weather/models/models.dart';
+import 'package:flutter_bloc_weather/repositories/repositories.dart';
+
+class WeatherRepository {
+  final WeatherApiClient weatherApiClient;
+
+  WeatherRepository({@required this.weatherApiClient})
+      : assert(weatherApiClient != null);
+
+  Future<Weather> getWeather(String city) async {
+    final int locationId = await weatherApiClient.getLocationId(city);
+    return await weatherApiClient.fetchWeather(locationId);
+  }
+}
